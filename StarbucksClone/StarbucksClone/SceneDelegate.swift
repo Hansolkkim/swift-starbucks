3a5fd95982fbdafb3dd724ea1c19b9fecba61a80
Sol <hskim6521@naver.com>
2022-05-10 17:14:50 +0900

    [Refactor] SceneDelegate 클래스 리팩토링 #4
    사용하지 않는 api 제거
    하드코딩된 appKey를 상수로 선언
    코드의 들여쓰기 단계를 줄이기 위해 if let 구문을 guard let 구문으로 수정

--- StarbucksClone/StarbucksClone/SceneDelegate.swift
+++ StarbucksClone/StarbucksClone/SceneDelegate.swift
@@ -5,29 +5,30 @@
 //  Created by 김동준 on 2022/05/09.
 //
 
-import UIKit
-import KakaoSDKUser
 import KakaoSDKAuth
 import KakaoSDKCommon
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    private let appKey = "9d618f74a6d63d44fe745da0f480233f"
     var window: UIWindow?
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        KakaoSDK.initSDK(appKey: "9d618f74a6d63d44fe745da0f480233f")
+        KakaoSDK.initSDK(appKey: appKey)
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = LoginViewController()
             self.window = window
             window.makeKeyAndVisible()
         }
-        guard let _ = (scene as? UIWindowScene) else { return }
     }
     
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-        if let url = URLContexts.first?.url {
-            if (AuthApi.isKakaoTalkLoginUrl(url)) {
-                _ = AuthController.handleOpenUrl(url: url)
-            }
+        guard let url = URLContexts.first?.url else {
+            return
         }
-        print("url \(URLContexts.first?.url)")
+
+        if AuthApi.isKakaoTalkLoginUrl(url) {
+            _ = AuthController.handleOpenUrl(url: url)
+        }
     }
 }
