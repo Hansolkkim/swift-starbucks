1d929bdbdefef0322861924b7ea22a73b9006d3f
Sol <hskim6521@naver.com>
2022-05-11 17:00:52 +0900

    [Feature] SceneDelegate에 makeSelectedViewController() 메소드 추가
    SceneDelegate 클래스에서 SceneUseCase의 selecteRootViewController의 반환값에 따라 rootViewController를 가지도록 하는 메서드 추가

--- StarbucksClone/StarbucksClone/SceneDelegate.swift
+++ StarbucksClone/StarbucksClone/SceneDelegate.swift
@@ -10,13 +10,15 @@ import KakaoSDKCommon
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     private let appKey = "9d618f74a6d63d44fe745da0f480233f"
+    private let sceneUseCase = SceneUseCase()
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         KakaoSDK.initSDK(appKey: appKey)
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = LoginViewController()
+            let selectedViewControllerType = sceneUseCase.selectRootViewController()
+            window.rootViewController = makeSelectedViewController(by: selectedViewControllerType)
             self.window = window
             window.makeKeyAndVisible()
         }
@@ -29,6 +31,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         if AuthApi.isKakaoTalkLoginUrl(url) {
             _ = AuthController.handleOpenUrl(url: url)
+        }
+    }
+}
+
+extension SceneDelegate {
+    private func makeSelectedViewController(by viewControllerType: ViewControllerType) -> UIViewController {
+        switch viewControllerType {
+        case .LoginViewController:
+            return LoginViewController()
+        case .EventViewController:
+            return LoginViewController() // EventViewController 구현 후 수정 예정
+        case .HomeViewController:
+            return LoginViewController() // HomeViewController 구현 후 수정 예정
         }
     }
 }
