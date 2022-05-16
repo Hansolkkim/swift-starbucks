5e5f1d709caf6284df6dfd554c96a87684345eba
Sol <hskim6521@naver.com>
2022-05-16 12:03:16 +0900

    [Refactor] UserDefaultManager 구조체 동작 별로 추상화 #14

--- StarbucksClone/StarbucksClone/Model/UserDefaultManager.swift
+++ StarbucksClone/StarbucksClone/Model/UserDefaultManager.swift
@@ -7,26 +7,45 @@
 
 import Foundation
 
+protocol Loginable {
+    func getStringFromUserDefault(by keyType: UserDefaultManager.KeyType) -> String?
+}
+
+protocol LoginDataSavable {
+    func saveLoginToken(_ tokenData: UserData)
+}
+
+protocol EventNeverSeeable {
+    func getBooleanFromUserDefault() -> Bool
+}
+
+protocol EventNeverSeeDataSavable {
+    func saveEventNeverSeeAgain()
+}
+
 struct UserDefaultManager {
     private let userDefault = UserDefaults.standard
 }
 
-extension UserDefaultManager {
+extension UserDefaultManager: Loginable {
     func getStringFromUserDefault(by keyType: KeyType) -> String? {
         return userDefault.string(forKey: keyType.key)
     }
-
+}
+extension UserDefaultManager: LoginDataSavable {
     func saveLoginToken(_ tokenData: UserData) {
         userDefault.set(tokenData.token, forKey: KeyType.userLoginToken.key)
         userDefault.set(tokenData.nickname, forKey: KeyType.userNickname.key)
     }
 }
 
-extension UserDefaultManager {
+extension UserDefaultManager: EventNeverSeeable {
     func getBooleanFromUserDefault() -> Bool {
         return userDefault.bool(forKey: KeyType.eventNeverSeeAgain.key)
     }
+}
 
+extension UserDefaultManager: EventNeverSeeDataSavable {
     func saveEventNeverSeeAgain() {
         userDefault.set(true, forKey: KeyType.eventNeverSeeAgain.key)
     }
